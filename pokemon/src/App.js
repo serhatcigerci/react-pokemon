fc22f889f80dc107b81f8b6e7115a77fd7fc8b5b
serhatcigerci <yserhatcigerci@gmail.com>
2023-07-29 17:50:28 +0300

    pulling the pokemons with axios

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import PokemonList from './PokemonList';
+import axios from 'axios'
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmender"])
+  const [pokemon, setPokemon] = useState([])
+  axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
+
   return (
-    <PokemonList pokemon={pokemon}/>
+    <PokemonList pokemon={pokemon}
+    />
   );
 }
 
