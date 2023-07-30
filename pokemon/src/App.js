00e7e23744b9625374bcd2ec707a4c5e56322f7b
serhatcigerci <yserhatcigerci@gmail.com>
2023-07-30 16:51:49 +0300

    little bit of css in buttons

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -37,8 +37,8 @@ function App() {
     <>
       <PokemonList pokemon={pokemon} />
       <Pagination 
-        goToNextPage={goToNextPage}
-        goToPrevPage={goToPrevPage}
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+        goToPrevPage={prevPageUrl ? goToPrevPage : null}
       />
     </>
   )
