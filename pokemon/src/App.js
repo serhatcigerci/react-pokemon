05efc247e9ca3092f3caf582c661027b97329d6d
serhatcigerci <yserhatcigerci@gmail.com>
2023-07-30 15:44:16 +0300

    Some css and background photo added

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PokemonList from './PokemonList';
 import axios from 'axios'
+import './App.css'
 
 function App() {
   const [pokemon, setPokemon] = useState([])
-  axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
-    setPokemon(res.data.results.map(p => p.name))
-  })
+
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+
+  useEffect(() => {
+    axios.get(currentPageUrl).then(res => {
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [currentPageUrl])
 
   return (
     <PokemonList pokemon={pokemon}
--- pokemon/src/index.js
+++ pokemon/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <div className='background'>
+      <div className='container'>
+        <div className="pokemon">
+          <App />
+        </div>
+      </div>
+    </div>
   </React.StrictMode>
 );
