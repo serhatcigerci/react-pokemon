5f5cefb7f9834afa0e7b7f7d192efa06bc09a441
serhatcigerci <yserhatcigerci@gmail.com>
2023-07-30 16:35:12 +0300

    Next and prev page urls on useEffect

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -7,12 +7,26 @@ function App() {
   const [pokemon, setPokemon] = useState([])
 
   const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get(currentPageUrl).then(res => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, { cancelToken: new axios.CancelToken(c => cancel = c) }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
+
+    return () => {
+      cancel.cancel()
+    }
   }, [currentPageUrl])
+
+  if(loading) return "Loading..."
 
   return (
     <PokemonList pokemon={pokemon}
